d2e12601b9b4be99337f2415d2fdb6e3eca806e0
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2018-11-02 11:36:16 -0300

    add redux-thunk and setting up thunk and logger middlewares

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import App from './components/App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+import middlewares from './middleware';
 
-const store = createStore(reducer);
+const store = createStore(reducer, middlewares);
 
 ReactDOM.render(
   <Provider store={store}>
