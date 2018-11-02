d39e3fff4dc573ed08b70ee8f304338fb32df34d
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2018-11-02 10:58:52 -0300

    defining actions

---
+++ src/actions/shared.js
@@ -0,0 +1,16 @@
+import { getInitialData } from '../utils/api';
+import { receiveUsers } from '../actions/users';
+import { receiveTweets } from '../actions/tweets';
+import { setAuthedUser } from '../actions/authedUser';
+
+const AUTHED_USER_ID = 'bruno_carpinelli';
+
+export function handleIntialData() {
+  return dispatch => {
+    return getInitialData().then(({ users, tweets }) => {
+      dispatch(receiveUsers(users));
+      dispatch(receiveTweets(tweets));
+      dispatch(setAuthedUser(AUTHED_USER_ID));
+    });
+  };
+}
